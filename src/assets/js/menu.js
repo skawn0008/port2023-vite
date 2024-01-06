b9a2c35800fa65897cdc7ca779babe44f92d8192
skawn0008 <skaxo0008@naver.com>
2024-01-07 08:37:12 +0900

    headerToggle.addEventListener(click , () => {

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,14 @@
 export function menu() {
-    console.log(menu)
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header_nav");
+
+   if(headerToggle){
+        headerToggle.addEventListener("click" , () => {
+            headerNav.classList.toggle("show");
+
+            headerToggle.getAttribute("aria-expanded") === "true"
+            ? headerToggle.setAttribute("aria-expanded", "false") 
+            : headerToggle.setAttribute("aria-expanded", "true");
+        });
+   }
 }
